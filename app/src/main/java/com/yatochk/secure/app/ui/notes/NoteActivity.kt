3b490edc2bc8f92319b0ef9b59add1ef2e26e6b5
zotov <CDr1546>
2019-10-21 01:20:08 +0400

    start notes

--- app/src/main/java/com/yatochk/secure/app/ui/notes/NoteActivity.kt
+++ app/src/main/java/com/yatochk/secure/app/ui/notes/NoteActivity.kt
@@ -1,5 +1,7 @@
 package com.yatochk.secure.app.ui.notes
 
+import android.os.Bundle
+import com.yatochk.secure.app.R
 import com.yatochk.secure.app.dagger.SecureApplication
 import com.yatochk.secure.app.ui.BaseActivity
 
@@ -7,6 +9,11 @@ class NoteActivity : BaseActivity() {
 
     override fun inject() {
         SecureApplication.appComponent.inject(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note)
     }
 
 }
