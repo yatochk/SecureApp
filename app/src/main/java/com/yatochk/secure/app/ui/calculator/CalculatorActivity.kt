6cdfcaac7c989062756b9312f9554201e34200c5
zotov <CDr1546>
2019-10-24 00:52:59 +0400

    add calculator layout

--- app/src/main/java/com/yatochk/secure/app/ui/calculator/CalculatorActivity.kt
+++ app/src/main/java/com/yatochk/secure/app/ui/calculator/CalculatorActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import com.yatochk.secure.app.R
 import com.yatochk.secure.app.dagger.SecureApplication
 import com.yatochk.secure.app.ui.BaseActivity
+import kotlinx.android.synthetic.main.activity_calculator.*
 
 class CalculatorActivity : BaseActivity() {
 
@@ -14,6 +15,10 @@ class CalculatorActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator)
+        keyboard.displayView = text_result
+        keyboard.setKeysListener {
+
+        }
     }
 
 }
--- app/src/main/java/com/yatochk/secure/app/ui/calculator/KeyboardView.kt
+++ app/src/main/java/com/yatochk/secure/app/ui/calculator/KeyboardView.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.widget.LinearLayout
+import android.widget.TextView
 import com.yatochk.secure.app.R
+import kotlinx.android.synthetic.main.view_keyboard.view.*
 
 class KeyboardView @JvmOverloads constructor(
     context: Context,
@@ -14,17 +16,88 @@ class KeyboardView @JvmOverloads constructor(
 
     init {
         View.inflate(context, R.layout.view_keyboard, this)
+        button_0.setOnClickListener {
+            onKeyClick(Key.KEY_0)
+        }
+        button_1.setOnClickListener {
+            onKeyClick(Key.KEY_1)
+        }
+        button_2.setOnClickListener {
+            onKeyClick(Key.KEY_2)
+        }
+        button_3.setOnClickListener {
+            onKeyClick(Key.KEY_3)
+        }
+        button_4.setOnClickListener {
+            onKeyClick(Key.KEY_4)
+        }
+        button_5.setOnClickListener {
+            onKeyClick(Key.KEY_5)
+        }
+        button_6.setOnClickListener {
+            onKeyClick(Key.KEY_6)
+        }
+        button_7.setOnClickListener {
+            onKeyClick(Key.KEY_7)
+        }
+        button_8.setOnClickListener {
+            onKeyClick(Key.KEY_8)
+        }
+        button_9.setOnClickListener {
+            onKeyClick(Key.KEY_9)
+        }
+        button_dot.setOnClickListener {
+            onKeyClick(Key.KEY_DOT)
+        }
+        button_equals.setOnClickListener {
+            onKeyClick(Key.KEY_EQUALS)
+        }
     }
 
-    private var listener: ((Int) -> Unit)? = null
+    private var listener: ((Key) -> Unit)? = null
 
-    fun setKeysListener(listener: (Int) -> Unit) {
+    fun setKeysListener(listener: (Key) -> Unit) {
         this.listener = listener
     }
 
-    private fun onKeyClick(key: Int) {
+    lateinit var displayView: TextView
+
+    private fun onKeyClick(key: Key) {
+        displayView.append(key.toString())
         listener?.invoke(key)
     }
 
+    enum class Key {
+        KEY_1,
+        KEY_2,
+        KEY_3,
+        KEY_4,
+        KEY_5,
+        KEY_6,
+        KEY_7,
+        KEY_8,
+        KEY_9,
+        KEY_0,
+        KEY_DOT,
+        KEY_EQUALS;
+
+        override fun toString(): String {
+            return when (this) {
+                KEY_0 -> "0"
+                KEY_1 -> "1"
+                KEY_2 -> "2"
+                KEY_3 -> "3"
+                KEY_4 -> "4"
+                KEY_5 -> "5"
+                KEY_6 -> "6"
+                KEY_7 -> "7"
+                KEY_8 -> "8"
+                KEY_9 -> "9"
+                KEY_DOT -> "."
+                KEY_EQUALS -> ""
+            }
+        }
+
+    }
 
 }
