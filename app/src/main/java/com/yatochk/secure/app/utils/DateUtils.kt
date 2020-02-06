8bc8cb82052344a21f43da6c11dea853ddb3f219
zotov <CDr1546>
2020-02-06 21:34:38 +0400

    change capture media name

--- app/src/main/java/com/yatochk/secure/app/utils/DateUtils.kt
+++ app/src/main/java/com/yatochk/secure/app/utils/DateUtils.kt
@@ -3,8 +3,9 @@ package com.yatochk.secure.app.utils
 import java.text.SimpleDateFormat
 import java.util.*
 
-private val timeFormat = SimpleDateFormat("yyyy-MM-dd-HH:mm_ss", Locale.ENGLISH)
+private val timeFormat = SimpleDateFormat("yyyy-MM-dd-HH-mm_ss", Locale.ENGLISH)
 
 fun Date.toTimeString(): String =
     timeFormat.format(this)
+
 
